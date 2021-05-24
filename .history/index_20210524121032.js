ea6ef0a17741bf1d143d68adba7fe1224bb7f27d
mouctare <mouctard78@gmail.com>
2021-05-24 12:20:13 +0200

    Calculs basiques

---
+++ .history/index_20210524121032.js
@@ -0,0 +1,35 @@
+let total = 0;
+
+function addition(x, y){
+    total = x + y;
+    return total;
+}
+
+function soustraction(x){
+    total -= x;
+    return total;
+}
+
+function division(x){
+    if(total === 0) {
+        return (total = x);
+    }else {
+
+        total /= x;
+        return total;
+    }
+}
+
+function multiplication(x){
+    if(total === 0) {
+        return (total = x);
+    }else {
+
+        total *= x;
+        return total;
+    }
+}
+
+function reset() {
+    total = 0;
+}
